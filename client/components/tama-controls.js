f9439a67957d6fbffec28d83150da0a7430e17ec
Jesus Trujillo <jesusft@verizonmedia.com>
2022-06-28 12:10:13 -0700

    round buttons and css vars

--- client/components/tama-controls.js
+++ client/components/tama-controls.js
@@ -45,8 +45,10 @@ export class TamaControls extends LitElement {
     super.firstUpdated()    
     this.canvasCtx = this.canvasRef.value.getContext('2d');
     this.canvasCtx.imageSmoothingEnabled = false;
-    this.iconRefs = this.renderRoot.querySelectorAll('.icon');
+    this.iconRefs = this.renderRoot.querySelectorAll('.icon');    
     this.canDraw = true;
+    this.canvasBackgroundColor = getComputedStyle(document.documentElement).getPropertyValue('--tama-screen-background') || 'rgb(0,0,0)';
+    this.canvasPixelColor = getComputedStyle(document.documentElement).getPropertyValue('--tama-screen-color') || 'rgb(255,255,255)';
   }
 
   onAnimationFrame() {
@@ -80,14 +82,14 @@ export class TamaControls extends LitElement {
     if (completeDraw) {
       for(let x = 0; x < TAMA_WIDTH; x++) {
         for(let y = 0; y < TAMA_HEIGHT; y++) {
-          canvas.fillStyle = this.lcd[x][y] > 0 ? 'rgb(0,0,0)' : 'rgb(255,255,255)';
+          canvas.fillStyle = this.lcd[x][y] > 0 ? this.canvasPixelColor : this.canvasBackgroundColor;
           canvas.fillRect(x, y, 1, 1);
         }
       }
     } else {
       this.lcdUpdates.forEach((n) => {
         const [x,y] = numberToPixel(n);
-        canvas.fillStyle = this.lcd[x][y] > 0 ? 'rgb(0,0,0)' : 'rgb(255,255,255)';
+        canvas.fillStyle = this.lcd[x][y] > 0 ? this.canvasPixelColor : this.canvasBackgroundColor;
         canvas.fillRect(x, y, 1, 1);
       });
       this.lcdUpdates.clear();
@@ -123,11 +125,6 @@ export class TamaControls extends LitElement {
 
   static get styles() {
     return css`
-      :root {
-        --tama-width: 400px;
-        --tama-background: bisque;
-      }
-      
       #canvas {
         width: var(--tama-width);
         aspect-ratio: 2/1;
@@ -137,6 +134,11 @@ export class TamaControls extends LitElement {
       .tama {
         width: var(--tama-width);
         background-color: var(--tama-background);
+        padding: 20px;
+      }
+
+      .screen {
+        background: var(--tama-screen-background);
       }
       
       .tama .icon-bar {
@@ -167,57 +169,36 @@ export class TamaControls extends LitElement {
 
       .interface-btn {
         aspect-ratio: 1/1;
-        background-color: #f7f7f7;
-        background-image: -webkit-gradient(linear, left top, left bottom, from(#f7f7f7), to(#e7e7e7));
-        background-image: -webkit-linear-gradient(top, #f7f7f7, #e7e7e7); 
-        background-image: -moz-linear-gradient(top, #f7f7f7, #e7e7e7); 
-        background-image: -ms-linear-gradient(top, #f7f7f7, #e7e7e7); 
-        background-image: -o-linear-gradient(top, #f7f7f7, #e7e7e7); 
-        border: solid 1px transparent;
+        background-color: var(--tama-button-gradient-from, #f7f7f7);
+        background-image: -webkit-gradient(linear, left top, left bottom, from(var(--tama-button-gradient-from, #f7f7f7)), to(var(--tama-button-gradient-to, #e7e7e7)));
+        background-image: -webkit-linear-gradient(top, var(--tama-button-gradient-from, #f7f7f7), var(--tama-button-gradient-to, #e7e7e7)); 
+        background-image: -moz-linear-gradient(top, var(--tama-button-gradient-from, #f7f7f7), var(--tama-button-gradient-to, #e7e7e7)); 
+        background-image: -ms-linear-gradient(top, var(--tama-button-gradient-from, #f7f7f7), var(--tama-button-gradient-to, #e7e7e7)); 
+        background-image: -o-linear-gradient(top, var(--tama-button-gradient-from, #f7f7f7), var(--tama-button-gradient-to, #e7e7e7)); 
+        border: var(--tama-button-border, solid 1px transparent);
         border-radius: 50%;
         box-shadow: 0px 3px 8px #aaa, inset 0px 2px 3px #fff;
         display: block;
         flex: 1;
         font-size: 1.5em;
         position: relative;
-        margin: 8%;
+        margin: var(--tama-button-spacing, 8%);
         text-weight: bold;
-      }
-
-      .interface-btn:link, .interface-btn:visited {
-        display: block;
-        text-decoration: none;
-        background-color: #f7f7f7;
-        background-image: -webkit-gradient(linear, left top, left bottom, from(#f7f7f7), to(#e7e7e7));
-        background-image: -webkit-linear-gradient(top, #f7f7f7, #e7e7e7); 
-        background-image: -moz-linear-gradient(top, #f7f7f7, #e7e7e7); 
-        background-image: -ms-linear-gradient(top, #f7f7f7, #e7e7e7); 
-        background-image: -o-linear-gradient(top, #f7f7f7, #e7e7e7); 
-        color: #a7a7a7;
-        margin: 36px;
-        width: 144px;
-        height: 144px;
-        position: relative;
-        text-align: center;
-        line-height: 144px;
-        border-radius: 50%;
-        box-shadow: 0px 3px 8px #aaa, inset 0px 2px 3px #fff;
-        border: solid 1px transparent;
       }
 
       .interface-btn:before {
         content: "";
         display: block;
-        background: #fff;
-        border-top: 2px solid #ddd;
+        background: var(--tama-button-inset-background, #fff);
+        border-top: 2px solid var(--tama-button-shadow-color, #ddd);
         position: absolute;
-        top: -18px;
-        left: -18px;
-        bottom: -18px;
-        right: -18px;
+        top: var(--tama-button-inset-size, -18px);
+        left: var(--tama-button-inset-size, -18px);
+        bottom: var(--tama-button-inset-size, -18px);
+        right: var(--tama-button-inset-size, -18px);
         z-index: -1;
         border-radius: 50%;
-        box-shadow: inset 0px 8px 48px #ddd;
+        box-shadow: inset 0px 8px 48px var(--tama-button-shadow-color, #ddd);
       }
 
       .interface-btn:active {
@@ -227,7 +208,7 @@ export class TamaControls extends LitElement {
       .interface-btn:hover {
         text-decoration: none;
         color: #555;
-        background: #f5f5f5;
+        background: var(--tama-button-hover-background, #f5f5f5);
       }
     `;
   }
@@ -235,18 +216,20 @@ export class TamaControls extends LitElement {
   render() {
     return html`
       <div class="tama">
-        <div class="icon-bar">
-          <img class="icon" src="assets/food.svg" alt="Food" data-icon=0 />
-          <img class="icon" src="assets/lights.svg" alt="Light" data-icon=1 />
-          <img class="icon" src="assets/games.svg" alt="Games" data-icon=2 />
-          <img class="icon" src="assets/medicine.svg" alt="Medicine" data-icon=3 />
-        </div>
-        <canvas ${ref(this.canvasRef)} id="canvas" width="32" height="16"></canvas>
-        <div class="icon-bar">
-          <img class="icon" src="assets/bath.svg" alt="Bath" data-icon=4 />
-          <img class="icon" src="assets/stats.svg" alt="Stats" data-icon=5 />
-          <img class="icon" src="assets/discipline.svg" alt="Discipline" data-icon=6 />
-          <img class="icon" src="assets/attention.svg" alt="Attention" data-icon=7 />
+        <div class="screen">
+          <div class="icon-bar">
+            <img class="icon" src="assets/food.svg" alt="Food" data-icon=0 />
+            <img class="icon" src="assets/lights.svg" alt="Light" data-icon=1 />
+            <img class="icon" src="assets/games.svg" alt="Games" data-icon=2 />
+            <img class="icon" src="assets/medicine.svg" alt="Medicine" data-icon=3 />
+          </div>
+          <canvas ${ref(this.canvasRef)} id="canvas" width="32" height="16"></canvas>
+          <div class="icon-bar">
+            <img class="icon" src="assets/bath.svg" alt="Bath" data-icon=4 />
+            <img class="icon" src="assets/stats.svg" alt="Stats" data-icon=5 />
+            <img class="icon" src="assets/discipline.svg" alt="Discipline" data-icon=6 />
+            <img class="icon" src="assets/attention.svg" alt="Attention" data-icon=7 />
+          </div>
         </div>
         <div class="buttons">
           <button class="interface-btn" @mousedown="${this._handleButton.bind(this, 0, 1)}" @mouseup="${this._handleButton.bind(this, 0, 0)}"></button>
